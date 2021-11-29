d0131b16bc96b4a8849b8adf626d5afe96622f84
wojciech-sak <wojciech.r.sak@gmail.com>
2021-11-29 07:01:15 +0100

    Refactor domain

--- src/main/java/com/kodilla/currencycalculator/domain/Currency.java
+++ src/main/java/com/kodilla/currencycalculator/domain/Currency.java
@@ -4,26 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Currency {
     private String table;
     private String currency;
     private String code;
     private List<Rates> rates;
-
-    @Override
-    public String toString() {
-        return "Currency{" +
-                "table='" + table + '\'' +
-                ", currency='" + currency + '\'' +
-                ", code='" + code + '\'' +
-                ", rates=" + rates +
-                '}';
-    }
 }
--- src/main/java/com/kodilla/currencycalculator/domain/Rates.java
+++ src/main/java/com/kodilla/currencycalculator/domain/Rates.java
@@ -4,20 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Rates {
     private String no;
     private String effectiveDate;
     private Double mid;
-
-    @Override
-    public String toString() {
-        return "Rates{" +
-                "mid=" + mid +
-                '}';
-    }
 }
